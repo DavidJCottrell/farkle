cb1bdc6c09f7a29b3b4726df9f404138dc69e4d1
davidjcottrell <davidjcottrell@icloud.com>
2021-05-31 19:42:30 +0100

    added working tests

---
+++ src/Farkle.Tests/Helpers.fs
@@ -0,0 +1,72 @@
+module Helpers
+
+open Xunit
+open FsCheck
+open FsCheck.Xunit
+open System
+
+open ScoringLibrary
+open GameLibrary
+
+let expectedPossibleSets:ScoreResults = [
+    SetCombination ([Dice 1; Dice 1; Dice 1], SetTotal 300);
+    SetCombination ([Dice 2; Dice 2; Dice 2], SetTotal 200);
+    SetCombination ([Dice 3; Dice 3; Dice 3], SetTotal 300);
+    SetCombination ([Dice 4; Dice 4; Dice 4], SetTotal 400);
+    SetCombination ([Dice 5; Dice 5; Dice 5], SetTotal 500);
+    SetCombination ([Dice 6; Dice 6; Dice 6], SetTotal 600);
+]
+
+let expectedPossibleRemainders:ScoreResults = [
+    RemainderCombination ([Dice 1], RemainderTotal 100);
+    RemainderCombination ([Dice 5], RemainderTotal 50);
+    RemainderCombination ([Dice 1; Dice 1], RemainderTotal 200);
+    RemainderCombination ([Dice 5; Dice 5], RemainderTotal 100);
+]
+
+
+let generateRandomRollWith1Remainder (diceForRemainder:Dice) =
+    let rand = Random()
+    let mutable valid = false
+    let mutable randomDice = []
+    
+    while not valid do
+        randomDice <- [
+            Dice (rand.Next (1, 7))
+            Dice (rand.Next (1, 7))
+            Dice (rand.Next (1, 7))
+            Dice (rand.Next (1, 7))
+            Dice (rand.Next (1, 7))
+        ]
+        if countOccurrences diceForRemainder randomDice = 0 then valid <- true
+    Gen.shuffle [diceForRemainder; randomDice.[0]; randomDice.[1]; randomDice.[2]; randomDice.[3]; randomDice.[4]] |> Gen.sample 0 6
+
+let generateRandomRollWith2Remainders (diceForRemainder:Dice) =
+    let rand = Random()
+    let mutable valid = false
+    let mutable randomDice = []
+    
+    while not valid do
+        randomDice <- [
+            Dice (rand.Next (1, 7))
+            Dice (rand.Next (1, 7))
+            Dice (rand.Next (1, 7))
+            Dice (rand.Next (1, 7))
+        ]
+        if countOccurrences diceForRemainder randomDice = 0 then valid <- true
+    let diceList:DiceList = [diceForRemainder; randomDice.[0]; randomDice.[1]; randomDice.[2]; randomDice.[3]; diceForRemainder]
+    Gen.shuffle diceList
+    |> Gen.sample 0 6
+    
+let generateRandomRollWith1Set (diceForSet:Dice) =
+    let rand = Random()
+    let mutable valid = false
+    let mutable randomDice = []
+    
+    // Generate 3 random dice (bar the case of 3 of the same dice as the input resulting in another set)
+    while not valid do
+        randomDice <- [Dice (rand.Next (1, 7)); Dice (rand.Next (1, 7)); Dice (rand.Next (1, 7))]
+        if countOccurrences diceForSet randomDice < 3 then valid <- true
+    
+    Gen.shuffle [diceForSet; diceForSet; diceForSet; randomDice.[0]; randomDice.[1]; randomDice.[2]] |> Gen.sample 0 6
+
--- src/Farkle.Tests/Tests.fs
+++ src/Farkle.Tests/Tests.fs
@@ -3,80 +3,49 @@ module Tests
 open Xunit
 open FsCheck
 open FsCheck.Xunit
+open System
+open Helpers
 
 open ScoringLibrary
 open GameLibrary
 
-let expectedPossibleSets:ScoreResults = [
-    SetCombination ([Dice 1; Dice 1; Dice 1], SetTotal 300);
-    SetCombination ([Dice 2; Dice 2; Dice 2], SetTotal 200);
-    SetCombination ([Dice 3; Dice 3; Dice 3], SetTotal 300);
-    SetCombination ([Dice 4; Dice 4; Dice 4], SetTotal 400);
-    SetCombination ([Dice 5; Dice 5; Dice 5], SetTotal 500);
-    SetCombination ([Dice 6; Dice 6; Dice 6], SetTotal 600);
-]
-
-let expectedPossibleRemainders:ScoreResults = [
-    RemainderCombination ([Dice 1], RemainderTotal 100);
-    RemainderCombination ([Dice 5], RemainderTotal 50);
-    RemainderCombination ([Dice 1; Dice 1], RemainderTotal 200);
-    RemainderCombination ([Dice 5; Dice 5], RemainderTotal 100);
-]
-
-// [<Fact>]
-// let ``A DiceList should never have more than (n - (n % 3)) / 3 sets`` (roll:DiceList) = 
-//     let len = List.length roll
-//     (len - (len % 3)) / 3
-
-
 [<Property>]
 let ``A DiceList should never have more than (n - (n % 3)) / 3 sets`` (roll:DiceList) = 
+    
     let scoreResults = scoreRoll roll
+    
     let mutable numOfSets = 0
     for score in scoreResults do
         match score with
         | SetCombination _ -> numOfSets <- numOfSets + 1
-        | _ -> numOfSets <- numOfSets + 1
-
-    let len = List.length roll
-    (len - (len % 3)) / 3
+        | _ -> ()
+    
+    let n = List.length roll
+    
+    numOfSets <= ((n - (n % 3)) / 3)
 
 [<Fact>]
-let ``test that the maximum number of sets for a given DiceList are never exceeded`` () = 
+let ``test that the maximum number of sets for a given DiceList are never exceeded`` () =
     let property num =
-        //roll some number of dice and check against property
-        rollDice num |> ``A DiceList should never have more than (n - (n % 3)) / 3 sets``
+        (rollDice (if num > 0 then num else 0)) |> ``A DiceList should never have more than (n - (n % 3)) / 3 sets``
+    Check.Quick property
     
-    Check.QuickThrowOnFailure property
 
-
-
-
-// Check.Verbose ``My test``
-
-// [<Property>]
-// let ``three identical dice return a SetCombination for that dice`` diceNum = 
-
-    // let dice = [Dice diceNum; Dice diceNum; Dice diceNum]
-    // let score = if diceNum = 1 then 300 else diceNum * 100
-    // (diceNum > 0 && diceNum < 7) ==> ((scoreRoll dice).[0] = SetCombination (dice, SetTotal score))
-
-// Check.Verbose ``three identical dice return a SetCombination for that dice``
-
-// let chooseFromList list = 
-//     gen { let! i = Gen.choose (0, List.length list-1) 
-//         return List.item i list } |> Gen.sample 0 10
-
-// [<Property>]
-// let ``a given roll should`` diceNum = 
-
-//     let set = (chooseFromList possibleSets).[0]
-    
-//     printfn "%A" set
-    
-//     //let setDice = (fst set)
-
-    
-//     true
-
-// Check.Verbose ``a given roll should``
+[<Fact>]
+let ``test that a SetCombination is returned by scoreRoll when given a DiceList with one set`` () =
+    // For each dice type
+    for i in [1..6] do
+        let total = if i = 1 then 300 else i * 100 // Get the total that dice will take as a set
+        let generatedSets = generateRandomRollWith1Set (Dice i) // Get a list of dice containing a set for that dice and some random other numbers
+        let expected = SetCombination([Dice i; Dice i; Dice i], SetTotal total) // Build the SetCombination the actual value should equate to
+        // GeneratedSets is a list of 6 combinations for the given dice
+        for setRoll in generatedSets do
+            let actualScores = scoreRoll (setRoll |> List.ofArray) // Score that generated DiceList
+            // Find the SetCombination from the resulting ScoreResults
+            let actualSet = List.find (fun score ->
+                    match score with
+                    | SetCombination (_) -> true
+                    | _ -> false) actualScores
+            // printfn $"Checking expected %A{expected} against actual  %A{actualSet}"
+            // Check it is equal to the expected
+            Assert.Equal(expected, actualSet)
